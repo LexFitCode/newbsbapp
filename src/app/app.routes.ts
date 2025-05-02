74251c4b2b97db75e0a154d1cf9676fbd45696fd
lex <alejandro.roqve@gmail.com>
2025-05-02 17:58:37 -0600

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
+import { HitterDetailsComponent } from './hitter-details/hitter-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'', component: HomeComponent, title:'home page'
+  },
+  {
+    path:'gameDetails/:id', component: GameDetailsComponent, title:'Game Details'
+  },
+  {
+    path:'hitterDetails/:id', component: HitterDetailsComponent, title:'Hitter Details'
+  }
+];
