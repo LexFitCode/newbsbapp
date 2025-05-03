26892f034a70d8b283b74a7bf313386d76735b14
lex <alejandro.roqve@gmail.com>
2025-05-02 18:02:09 -0600

    hitter component

---
+++ src/app/hitter-details/hitter-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hitter-details',
+  imports: [],
+  templateUrl: './hitter-details.component.html',
+  styleUrl: './hitter-details.component.scss'
+})
+export class HitterDetailsComponent {
+
+}
