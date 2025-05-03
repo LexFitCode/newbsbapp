f3e03aa42c93bd71db82da9b88b1031600628f7f
lex <alejandro.roqve@gmail.com>
2025-05-02 18:04:46 -0600

    pitcher component

---
+++ src/app/pitcher-details/pitcher-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pitcher-details',
+  imports: [],
+  templateUrl: './pitcher-details.component.html',
+  styleUrl: './pitcher-details.component.scss'
+})
+export class PitcherDetailsComponent {
+
+}
