c6745973a68c9f9867e5132932e6ae009fec8180
lex <alejandro.roqve@gmail.com>
2025-05-02 18:00:51 -0600

    game component

---
+++ src/app/game-details/game-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-game-details',
+  imports: [RouterModule],
+  templateUrl: './game-details.component.html',
+  styleUrl: './game-details.component.scss'
+})
+export class GameDetailsComponent {
+
+}
